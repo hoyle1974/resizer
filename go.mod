a9aac80b37ee66fecd6bfd1b8e185758baee0f90
Jack Strohm <hoyle.hoyle@gmail.com>
2023-03-15 16:29:41 -0700

    Initial checkin

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/hoyle1974/resizer
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0
+
+require github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/nfnt/resize"
+)
+
+func writeErr(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Header().Set("Content-Type", "text")
+	w.Write([]byte(err.Error()))
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) error {
+	width, err := strconv.Atoi(r.URL.Query().Get("width"))
+	if err != nil {
+		return fmt.Errorf("Width: %w", err)
+	}
+	height, err := strconv.Atoi(r.URL.Query().Get("height"))
+	if err != nil {
+		return fmt.Errorf("Height: %w", err)
+	}
+
+	fmt.Printf("Width: %v  Height: %v\n", width, height)
+
+	// Maximum upload of 10 MB files
+	err = r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		return fmt.Errorf("ParseMultipartForm: %w", err)
+	}
+
+	for key, _ := range r.MultipartForm.File {
+		fmt.Printf("File: %v\n", key)
+		// Get handler for filename, size and headers
+		file, handler, err := r.FormFile(key)
+		if err != nil {
+			return fmt.Errorf("Error Retrieving the File: %w", err)
+		}
+
+		defer file.Close()
+		fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+		fmt.Printf("File Size: %+v\n", handler.Size)
+		fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+		imageData, imageType, err := image.Decode(file)
+		if err != nil {
+			return fmt.Errorf("image.Decode: %w", err)
+		}
+		fmt.Printf("Image Type: %v\n", imageType)
+
+		newImage := resize.Resize(uint(width), uint(height), imageData, resize.Lanczos3)
+
+		imageBytes := new(bytes.Buffer)
+		jpeg.Encode(imageBytes, newImage, nil)
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Write(imageBytes.Bytes())
+		return nil
+	}
+
+	return fmt.Errorf("No image found")
+}
+
+func ReceiveFile(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		//display(w, "upload", nil)
+	case "POST":
+		err := uploadFile(w, r)
+		if err != nil {
+			log.Println(err)
+			writeErr(w, err)
+		}
+	}
+
+	return
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.
+		Path("/resize").
+		Methods("POST").
+		HandlerFunc(ReceiveFile)
+	log.Println("Starting Resizer . . .")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
